86381f5ecab7d3f9c2953b7147c234a2348f8107
林岑影 <lincenying@qq.com>
2024-03-28 15:39:10 +0800

    chore: 修改管理后台文章类别默认排序

--- src/api/backend-article.ts
+++ src/api/backend-article.ts
@@ -36,10 +36,10 @@ function marked(content: string) {
  * @param req Request
  * @param res Response
  */
-export async function getList(req: Req<{ page: string; limit: string }>, res: Res) {
+export async function getList(req: Req<{ page: string; limit: string; sort: string }>, res: Res) {
     let json: ResData<Nullable<Lists<Article[]>>>
 
-    const sort = '-_id'
+    const sort = req.query.sort || '-update_date'
     const page = Number(req.query.page) || 1
     const limit = Number(req.query.limit) || 10
     const skip = (page - 1) * limit
