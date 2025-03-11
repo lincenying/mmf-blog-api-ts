8afa6e80182563a21060b0d97ade55172d24abcc
林岑影 <lincenying@qq.com>
2025-03-11 15:06:50 +0800

    chore: 使用pnpm-workspace

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,6 +3,7 @@ import lincy from '@lincy/eslint-config'
 const config = lincy(
     {
         vue: false,
+        pnpm: true,
         overrides: {
             ignores: [
                 '**/assets',
